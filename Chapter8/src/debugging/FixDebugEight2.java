44c46973bb7482363faf483f08f4142f1c422f72
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-10-09 09:40:14 -0500

    Debugging

--- Chapter8/src/debugging/FixDebugEight2.java
+++ Chapter8/src/debugging/FixDebugEight2.java
@@ -1,9 +1,17 @@
 package debugging;
 
-public class FixDebugEight2 {
+public class FixDebugEight2 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		 int[] someNums = {4, 17, 22, 8, 35};
+	      int tot = 0;
+	      int x;
+	      for(x = 0; x < someNums.length; --x)
+	         tot += someNums[x];
+	      System.out.println("Sum is " + tot);
+	      System.out.println("Average is " + tot * 1.0 / someNums.length);
 
 	}
 
